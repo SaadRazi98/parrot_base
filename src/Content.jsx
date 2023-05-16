34ac5047739cd696bd2f078d2f1d5e755a81ba40
SAAD <SAADRAZI1998@GMAIL.com>
2023-05-16 15:26:14 -0500

    Back end with some minor issues

--- src/Content.jsx
+++ src/Content.jsx
@@ -3,6 +3,9 @@ import { useState, useEffect } from "react";
 import { PhotosIndex } from "./PhotosIndex";
 import { PhotosShow } from "./PhotosShow";
 import { Modal } from "./Modal";
+import { Signup } from "./Signup";
+import { Login } from "./Login";
+import { LogoutLink } from "./LogoutLink";
 
 export function Content() {
   const [photos, setPhotos] = useState([]);
@@ -23,6 +26,26 @@ export function Content() {
     setCurrentPhoto(photo);
   };
   
+  const handleUpdatePhoto = (id, params, successCallback) => {
+      console.log("handleUpdatePhoto", params);
+      axios.patch(`http://localhost:3000/photos/${id}.json`, params).then((response) => {
+        setPhotos(
+          photos.map((photo) => {
+            if (photo.id === response.data.id) {
+              return response.data;
+            } else {
+              return photo;
+            }
+          })
+        );
+        successCallback();
+        handleClose();
+      });
+    };
+
+
+
+
   const handleClose = () => {
     console.log("handleClose");
     setIsPhotosShowVisible(false);
@@ -33,12 +56,19 @@ export function Content() {
   
   
   return (
-    <div>
+    <div className ="container">
+      <Signup />
+      <Login />
+      <LogoutLink />
+      <PhotosNew onCreatePhoto={handleCreatePhoto}/>
+      <br />
+      <br />
+      <br />
       <h1>Welcome to Parrot Base!</h1>
       <PhotosIndex photos={photos} onShowPhoto={handleShowPhoto} />
       <Modal show={isPhotosShowVisible} onClose={handleClose}>
-        <PhotosShow photo={currentPhoto} />
+          <PhotosShow photo={currentPhoto} onUpdatePhoto={handleUpdatePhoto} />
         </Modal>
     </div>
-  )
+  );
 }
--- src/PhotosShow.jsx
+++ src/PhotosShow.jsx
@@ -1,4 +1,12 @@
 export function PhotosShow(props) {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    props.onUpdatePhoto(props.photo.id, params, () => event.target.reset());
+  };
+
+
   return (
     <div>
       <h1>Photo information</h1>
@@ -6,6 +14,21 @@ export function PhotosShow(props) {
       <p>Url: {props.photo.url}</p>
       <p>Width: {props.photo.width}</p>
       <p>Height: {props.photo.height}</p>
-    </div>
-  );
-}
+       <form onSubmit={handleSubmit}>
+         <div>
+           Name: <input defaultValue={props.photo.name} name="name" type="text" />
+         </div>
+         <div>
+           Url: <input defaultValue={props.photo.url} name="url" type="text" />
+         </div>
+         <div>
+           Width: <input defaultValue={props.photo.width} name="width" type="text" />
+         </div>
+         <div>
+           Height: <input defaultValue={props.photo.height} name="height" type="text" />
+         </div>
+         <button type="submit">Update photo</button>
+       </form>
+      </div>
+    );
+  }
